44948f3b3c6f6f8b65f08b6a5d21f802c9dc7ba4
douglas.araujo <doug.ramalho@gmail.com>
2017-06-20 18:08:44 -0300

    Fixed authorize control

--- source/client/src/components/Require_authentication.js
+++ source/client/src/components/Require_authentication.js
@@ -1,10 +1,27 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types";
 import {connect} from "react-redux";
 
 export default function(ComposedComponent){
     class Authentication extends Component {
+        
+        static contextTypes = {
+            router: PropTypes.object
+        }
+
+        componentWillMount(){
+            if(!this.props.authenticated){
+                this.context.router.history.push("/");
+            }
+        }
+
+        componentWillUpdate(nextProps){
+            if(!nextProps.authenticated){
+                this.context.router.history.push("/");
+            }
+        }
+        
         render(){
-            console.log(this.props.authenticated);
             return <ComposedComponent {...this.props} />
         }
     }
